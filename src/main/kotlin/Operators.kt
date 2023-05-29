2bb827b2ef942a3ff16e8a4b4886c5c8ff3202c6
Dipesh Yadav <dipeshyadav4444@gmail.com>
2023-05-29 23:44:42 +0530

    operator added

---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val a = 5
+    val b = 2
+
+    var c = a + b
+    println(c)
+
+    c = a - b
+    println(c)
+
+    c = a * b
+    println(c)
+
+    c = a / b
+    println(c)
+
+    c = a % b
+    println(c)
+
+    println(a.toDouble() / b)
+    println(a.toDouble() % b.toDouble())
+}
