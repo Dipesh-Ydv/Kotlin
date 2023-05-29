715b058ea7e175b98a7093bcc1a9cd5a354fde85
Dipesh Yadav <dipeshyadav4444@gmail.com>
2023-05-29 23:47:23 +0530

    operator added

--- src/main/kotlin/Operators.kt
+++ src/main/kotlin/Operators.kt
@@ -2,18 +2,23 @@ fun main() {
     val a = 5
     val b = 2
 
+    // Addition operator
     var c = a + b
     println(c)
 
+    // Subtraction operator
     c = a - b
     println(c)
 
+    // Multiplication operator
     c = a * b
     println(c)
 
+    // Division operator
     c = a / b
     println(c)
 
+    // Modulo operator
     c = a % b
     println(c)
 
